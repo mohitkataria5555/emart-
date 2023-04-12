b5be2fb367df3449b74703779e7e8f81239fec28
mohitkataria5555 <mohitkataria5555@gmail.com>
2023-04-12 20:37:08 +0530

    cart testcases

--- cart/src/test/kotlin/com/axis/cart/CartControllerTests.kt
+++ cart/src/test/kotlin/com/axis/cart/CartControllerTests.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -50,6 +51,34 @@ fun getAllCarts(){
     Assertions.assertThat(response).isNotNull
     Assertions.assertThat(response).contains(cart1,cart2)
 }
+    @Test
+    fun getCartById(){
+        webTestClient.get().uri("/carts/{mobileNo}", cart1.getMobileNo())
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(Cart::class.java)
+            .returnResult().responseBody
+    }
+    @Test
+    fun deleteCartById(){
+        webTestClient.delete().uri("/carts/delete/{mobileNo}",cart1.getMobileNo())
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(Cart::class.java)
+            .returnResult().responseBody
+    }
+
+    @Test
+    fun createCart(){
+        webTestClient.post().uri("/carts/add",cart1.getMobileNo())
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(cart1)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(Cart::class.java)
+            .isEqualTo(cart1)
+            .returnResult()
+    }
 
 
 
